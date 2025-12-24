47858b75872e3a16ea46ac26099d10b67da5c267
Gabriel Gratival <gabriel.gratival@gmail.com>
2025-12-24 13:34:56 +0000

    altera a página `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Luri eu te amo. Se vc me ama, dá um sorriso. =)</h1>
+    return <h1>Luri eu te amo. Se vc tá afim, dá uma risadinha. =P</h1>
 }
 
 export default Home;
