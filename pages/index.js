2d7598c6361c14bb067815449eb0ee0810cc1de3
Gabriel Gratival <gabriel.gratival@gmail.com>
2025-12-24 14:42:39 +0000

    adiciona script lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Luri eu te amo. Se vc tá afim, dá uma risadinha. =P</h1>
+function Home() {
+  return <h1>Luri eu te amo. Se vc tá afim, dá uma risadinha. =P</h1>;
 }
 
 export default Home;
