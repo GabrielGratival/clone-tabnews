bf46157a1b93369281c2ebba55433bdd57e46f08
Gabriel Gratival <gabriel.gratival@gmail.com>
2025-12-28 00:42:32 +0000

    add `.env` and `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
+import database from "../../../../infra/database.js";
+
 function status(request, response) {
+  const result = database.query("SELECT NOW()");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
 
